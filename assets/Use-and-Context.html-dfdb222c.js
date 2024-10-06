def57e3d56b0ad1c681709db49afce0a254cb096
guomaimang <47519540+guomaimang@users.noreply.github.com>
2024-10-06 10:24:26 +0000

    Deploying to gh-pages from @ guomaimang/guomaimang.github.io@0a8bd687c6548ac61fdb014af1071bc9c50a1c95 🚀

--- assets/Use-and-Context.html-dfdb222c.js
+++
@@ -1,9 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as i,e as t}from"./app-811b5c0c.js";const a={},s=t(`<h1 id="use-and-context" tabindex="-1"><a class="header-anchor" href="#use-and-context" aria-hidden="true">#</a> Use and Context</h1><h2 id="history-of-hci" tabindex="-1"><a class="header-anchor" href="#history-of-hci" aria-hidden="true">#</a> History of HCI</h2><img src="https://pic.hanjiaming.com.cn/2022/12/06/f9149976671b7.png" alt="CleanShot 2022-12-06 at 19.33.48@2x.png" style="zoom:50%;"><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>What is the role of the user?
-The role of the user is ...
-
-What does the interaction look like?
-The interaction is ...
-
-What is the relation between human and machine?
-The relation between human and machine is ...
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container tip"><p class="hint-container-title">道格拉斯·恩格尔巴特（1950 年代）</p><p>“……世界越来越复杂，问题越来越紧迫。这些必须集体处理。然而，人类集体处理复杂/紧迫问题的能力并没有像这些问题那样快速增长。</p><p>如果你能做些什么来提高人类处理这些问题的能力，那么你真的会做出一些基本的贡献。”</p></div><div class="hint-container info"><p class="hint-container-title">增强的概念框架</p><p>“通过增强人的智力，我们的意思是增加人处理复杂问题情况的能力，获得适合他的特殊需要的理解力，并得出问题的解决方案。</p><p>一个目标是开发新的技术、程序和系统，使人们的基本信息处理能力更好地适应社会的需要、问题和进步。</p></div><p>人机交互导致成本降低（减少重新设计） 人机交互是卖点（否则为什么买苹果？ 避免灾难（金融、航天、核、自动驾驶汽车）。</p><p>历史：从很少的互动和人对系统的服从，到大量的互动和系统对人类需求的照顾，有远见的人想出了下一代的互动模式。珍惜你的极客!</p>`,8),d=[s];function c(o,r){return n(),i("div",null,d)}const p=e(a,[["render",c],["__file","Use-and-Context.html.vue"]]);export{p as default};
