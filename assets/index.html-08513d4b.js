2db65ee492dde98156860981e754f32b258a332f
guomaimang <47519540+guomaimang@users.noreply.github.com>
2024-11-20 16:56:06 +0000

    Deploying to gh-pages from @ guomaimang/guomaimang.github.io@8ce758d7e56a7d2e75913bddb8b7a0a0d112963d 🚀

---
+++ assets/index.html-08513d4b.js
@@ -0,0 +1,46 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as a,f as l,a as i,b as d,e as s}from"./app-da918afa.js";const r={},c=i("h1",{id:"人机交互",tabindex:"-1"},[i("a",{class:"header-anchor",href:"#人机交互","aria-hidden":"true"},"#"),d(" 人机交互")],-1),t=i("p",null,"Human Computer interaction",-1),m=i("ul",null,[i("li",null,"使你的设计立足于现实的方法"),i("li",null,"原型化（视觉）应用的方法"),i("li",null,"屏幕设计和表示的基础知识"),i("li",null,"评价界面质量的方法"),i("li",null,"如何将准则应用于界面设计"),i("li",null,"有足够的背景来继续你作为交互设计师的教育")],-1),u=s(`<figure><img src="https://pic.hanjiaming.com.cn/2022/12/04/5d36002fa771d.png" alt="1670168635628.png" tabindex="0" loading="lazy"><figcaption>1670168635628.png</figcaption></figure><h2 id="exam-item" tabindex="-1"><a class="header-anchor" href="#exam-item" aria-hidden="true">#</a> Exam item</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>亲爱的交互设计师:
+
+以下是对你的考试内容的总结。考试是开放性问题和选择题（MC）的混合。
+我们还在讨论每种题型的数量。
+幻灯片是领先的。我们不会问书中的微小细节。考试项目大致分为3类。
+
+知识: 什么是运动视差？
+应用: 在这个VR应用中，运动视差是如何影响用户的感知的？
+洞察力: 运动视差与这种和那种类型的模式识别有什么关系？
+
+对于开放性问题，不要试图写出尽可能多的内容，让你的老师决定哪些部分构成正确的答案。
+这基本上意味着你只是背诵但没有理解内容。
+要简明扼要，准确无误。更多的字数并不能带来更多的分数。
+
+MC有4个答案选项。通常，并非总是如此，其中之一完全是胡说八道。
+其中之一是最精确、最完整和最贴切的。
+这是唯一正确的答案。其余的可能很接近但还不够好。
+
+问题是用文字包装起来的，就像现实生活一样。
+你必须好好阅读并理解所描述的问题和情况。然后人们会给你不同的答案。
+由你来决定谁在胡说八道，谁是对的。
+
+谢谢你在过去三个月里的关注。很高兴能教你。
+
+成功的机会很多!
+
+约翰和杰夫
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="importance-of-hci" tabindex="-1"><a class="header-anchor" href="#importance-of-hci" aria-hidden="true">#</a> Importance of HCI</h2><p>Computer-Human Interaction (CHI)</p><ul><li>User-Centred Design</li><li>Interaction Design</li></ul><img src="https://pic.hanjiaming.com.cn/2022/12/04/f5d6c743ecf46.png" alt="1670169396588.png" style="zoom:33%;"><p>63%的大型软件项目超过了成本，管理人员给出了四个与用户有关的理由：</p><ul><li>用户请求更改</li><li>忽视任务</li><li>Overlooking tasks「忽视任务」</li><li>用户不了解自己的需求</li><li>用户与开发者之间的沟通和理解不够充分</li></ul><p>可用性工程「Usability engineering」就是软件工程</p><ul><li>编码员过快地跳入详细设计</li><li>对话流程不当</li><li>不容易使用</li><li>永远不会被测试，直到它为时已晚</li></ul><p>现在付一点，或者以后付很多！</p><img src="https://pic.hanjiaming.com.cn/2022/12/06/58cae5f1b57b9.png" alt="1670321986970.png" style="zoom:50%;"><img src="https://pic.hanjiaming.com.cn/2022/12/06/e9c44361f3d1e.png" alt="1670322236443.png" style="zoom:50%;"><figure><img src="https://pic.hanjiaming.com.cn/2022/12/06/b99bcfe64770b.png" alt="1670325193076.png" tabindex="0" loading="lazy"><figcaption>1670325193076.png</figcaption></figure><div class="hint-container warning"><p class="hint-container-title">人机交互是否只是关于界面设计？</p><p>不，人机交互也关注客户旅程、工作组织、任务授权、业务流程、算法；任何可以改善用户体验的东西。</p></div><ul><li>Ergonomic design「人体工程学设计」</li><li>减少短期记忆负荷</li><li>提供错误预防</li><li>允许轻松逆转行动</li><li>提供丰富的反馈信息</li><li>支持内部控制权</li></ul><h2 id="definitions" tabindex="-1"><a class="header-anchor" href="#definitions" aria-hidden="true">#</a> Definitions</h2><ul><li>&quot;人机交互......关注的是供人类使用的交互式计算系统的设计、评估和实施，以及对围绕这些系统的主要现象的研究。&quot;(ACM SIGCHI的人机交互课程)</li><li>&quot;用户的输入语言，机器的输出语言，以及互动的协议&quot; (CHI 1985)</li><li>&quot;它只是你看到、听到、触摸或交谈的计算机部分。它是使你和你的计算机能够相互交流的所有东西的集合&quot;。(IBM设计概念)</li><li>一门与人类使用的交互式计算系统的设计、实施和评估有关的学科</li></ul><img src="https://pic.hanjiaming.com.cn/2022/12/07/e2aecb05a8ea4.png" alt="1670394981841.png" style="zoom:33%;"><h2 id="exam-example" tabindex="-1"><a class="header-anchor" href="#exam-example" aria-hidden="true">#</a> Exam Example</h2><h3 id="example-1" tabindex="-1"><a class="header-anchor" href="#example-1" aria-hidden="true">#</a> Example 1</h3><figure><img src="https://pic.hanjiaming.com.cn/2022/12/07/917ce678217ea.png" alt="1670395737930.png" tabindex="0" loading="lazy"><figcaption>1670395737930.png</figcaption></figure><h3 id="example-2" tabindex="-1"><a class="header-anchor" href="#example-2" aria-hidden="true">#</a> Example 2</h3><figure><img src="https://pic.hanjiaming.com.cn/2022/12/06/20aa27a2e1e6d.png" alt="1670328501446.png" tabindex="0" loading="lazy"><figcaption>1670328501446.png</figcaption></figure><h3 id="example-3" tabindex="-1"><a class="header-anchor" href="#example-3" aria-hidden="true">#</a> Example 3</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>简答题，参考LEC1
+荷航解释说 Expedia 取消了预订，因为我是在他们那里买的机票 荷航无法处理代理人出售的机票
+这个系统的人机交互设计有什么问题？
+
+1. Flying Blue（航空里程系统）应该声明您不能使用里程升舱
+2. KLM 网站应与代理商核实他们的机票预订地点（并警告想要不可能的事情的用户）
+3. Expedia 应通知其客户有关预订状态的更改
+4. KLM 无法处理 Expedia 机票是业务流程的一部分，不应打扰您的客户
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="example-4" tabindex="-1"><a class="header-anchor" href="#example-4" aria-hidden="true">#</a> Example 4</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>What is wrong with Mac’s design? 
+Think of control processes, feedback, labels, explanations
+
+1. 无论任务如何，都会切断电源（不检查正在进行的进程）。
+2. 在事件发生前不提供任何警告反馈（入睡）。
+3. 在事件发生后不提供反馈（它不知道出了问题）。
+4. 没有声明 &quot;睡眠 &quot;的实际含义或其原因（这是一个没有解释的隐喻）。
+5. 标签含糊不清：同一个标签表示多种内容（电池信息、电池设置）。
+6. Violation of the 3-click rule
+7. Instructions only through Internet (because cheap)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="reference" tabindex="-1"><a class="header-anchor" href="#reference" aria-hidden="true">#</a> Reference</h2><ul><li>COMP3423 2022 Fall@PolyU</li></ul>`,31);function o(v,h){return n(),a("div",null,[c,t,m,l(" more "),u])}const b=e(r,[["render",o],["__file","index.html.vue"]]);export{b as default};
