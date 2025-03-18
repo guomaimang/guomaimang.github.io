134b30312fb44ed519fcf9b0d9836ab08862a261
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2025-03-19 03:53:43 +0800

    update

---
+++ src/.vuepress/client.ts
@@ -0,0 +1,15 @@
+import { defineClientConfig } from "@vuepress/client";
+
+export default defineClientConfig({
+  enhance({ router }) {
+    // 添加路由钩子
+    router.beforeEach((to) => {
+      // 如果访问的是 /myself.html，则重定向到外部URL
+      if (to.path === "/myself.html") {
+        // 使用window.location进行重定向
+        window.location.href = "/myself/index.html";
+        return false; // 终止导航
+      }
+    });
+  },
+}); 
