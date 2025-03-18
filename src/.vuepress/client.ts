8d140be27604f8c578a9656c2506a6be59dc0a81
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2025-03-19 04:04:24 +0800

    update

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -6,9 +6,12 @@ export default defineClientConfig({
     router.beforeEach((to) => {
       // 如果访问的是 /myself.html，则重定向到外部URL
       if (to.path === "/myself.html") {
-        // 使用window.location进行重定向
-        window.location.href = "/myself/index.html";
-        return false; // 终止导航
+        // 检查是否在客户端环境
+        if (typeof window !== "undefined") {
+          // 使用window.location进行重定向
+          window.location.href = "/myself/index.html";
+          return false; // 终止导航
+        }
       }
     });
   },
