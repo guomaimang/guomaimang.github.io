ec433808c16baabaa5e595f23bac97ca5c62c36b
guomaimang <47519540+guomaimang@users.noreply.github.com>
2026-02-16 11:41:06 +0000

    Deploying to gh-pages from @ guomaimang/guomaimang.github.io@a6522db95cc2e7b6734f0c94c4f253e21af3f78b 🚀

---
+++ assets/shell.html-93eb81f6.js
@@ -0,0 +1,8 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as a,c as l,e as n}from"./app-c5b46a43.js";const i={},d=n(`<h1 id="shell" tabindex="-1"><a class="header-anchor" href="#shell" aria-hidden="true">#</a> Shell</h1><h2 id="常用命令-程序" tabindex="-1"><a class="header-anchor" href="#常用命令-程序" aria-hidden="true">#</a> 常用命令/程序</h2><h3 id="echo" tabindex="-1"><a class="header-anchor" href="#echo" aria-hidden="true">#</a> echo</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>$ echo Hello world
+Hello world
+
+$ echo Hello world \\
+on a long line
+
+Hello world on a long line
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="cal" tabindex="-1"><a class="header-anchor" href="#cal" aria-hidden="true">#</a> cal</h3><img src="https://pic.hanjiaming.com.cn/2022/05/01/2c84b457a7297.png" alt="1651338326122.png" style="zoom:50%;">`,6),r=[d];function s(c,o){return a(),l("div",null,r)}const m=e(i,[["render",s],["__file","shell.html.vue"]]);export{m as default};
